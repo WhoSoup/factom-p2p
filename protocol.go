25527dd5a4f7451634d7b7b0bfc839e17e19128f
Who Soup <who.soup@gmail.com>
2019-05-08 14:55:12 +0200

    comment

--- protocol.go
+++ protocol.go
@@ -1,5 +1,15 @@
 package p2p
 
+// Protocol is the interface for reading and writing parcels to
+// the underlying connection. The job of a protocol is to encode a Parcel
+// and send it over TCP to another instance of the same Protocol on the
+// other end
+//
+// Send:    Parcel => Protocol Encoder => Protocol Format => TCP
+// Receive: TCP => Protocol Format => Protocol Decoder => Parcel
+//
+// Every protocol should be bootstraped in peer:bootstrapProtocol() where
+// it can be initialized with the required serialization methods
 type Protocol interface {
 	Send(p *Parcel) error
 	Receive() (*Parcel, error)
